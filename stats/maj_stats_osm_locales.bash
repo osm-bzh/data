844bbdc618039b15cd652b3a1f7a8aebc1f5734c
MaelREBOUX <m.reboux@rennesmetropole.fr>
2021-09-13 23:34:56 +0200

    Update maj_stats_osm_locales.bash

--- stats/maj_stats_osm_locales.bash
+++ stats/maj_stats_osm_locales.bash
@@ -6,10 +6,10 @@
 # 1. exécuter un script python pour faire des stats sur taginfo de OSM
 # 2. commiter et puller le fichier de stats mis à jour
 
+cd /data/projets/osm-bzh-data/stats
+
 # lecture du fichier de config pour les infos github
 . config.sh
-
-cd /data/projets/osm-bzh-data/stats
 
 source venv/bin/activate
 python3 locale_names_monitor.py
@@ -17,6 +17,6 @@ python3 locale_names_monitor.py
 git add stats_locale_names.csv
 git commit -m 'maj mensuelle automatique'
 
-git push 'https://{github_user}:{github_token}@github.com/osm-bzh/osm-bzh-data.git' master
+git push 'https://$github_user:$github_token@github.com/osm-bzh/osm-bzh-data.git' master
 
 deactivate
